87c86b1190c0f55530c51c68be22189d2517fb1c
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2026-03-12 22:23:31 -0300

    TagChip component created

--- components/items/tag-chips.tsx
+++ components/items/tag-chips.tsx
@@ -1,31 +1,29 @@
+import { useTags } from "@/hooks/use-tags";
 import { cn } from "@/lib/utils";
+import { useEffect } from "react";
 
-//TODO: Depois precisamos trocar pelas tags
-const CATEGORIES = [
-  "All",
-  "Quick notes",
-  "To-do list",
-  "Journal",
-  "Tags",
-  "Random",
-] as const;
-
-export default function CategoryChips({
+export default function TagChips({
   value,
   onChange,
 }: {
   value: string;
-  onChange: (category: string) => void;
+  onChange: (tag: string) => void;
 }) {
+    const userId = "user_teste_123";
+    const { fetchTags, tags } = useTags();
+    useEffect(() => {
+      fetchTags(userId);
+    }, [fetchTags, userId]);
+
   return (
     <div className="flex w-full items-center justify-start gap-1.5 overflow-x-auto bg-transparent px-2 pb-2 [&::-webkit-scrollbar]:hidden md:p-1.5 md:pb-1.5">
-      {CATEGORIES.map((category) => {
-        const active = category === value;
+      {tags.map((tag) => {
+        const active = tag.title === value;
         return (
           <button
-            key={category}
+            key={tag.id}
             type="button"
-            onClick={() => onChange(category)}
+            onClick={() => onChange(tag.title)}
             className={cn(
               "shrink-0 rounded-full px-4 py-2 text-[12px] font-semibold transition-all duration-200",
               active
@@ -33,7 +31,7 @@ export default function CategoryChips({
                 : "bg-muted text-accent-foreground",
             )}
           >
-            {category}
+            {tag.title}
           </button>
         );
       })}
