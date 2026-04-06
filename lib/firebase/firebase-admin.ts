9e382aa9610d6a5c2781bc858ce65ddbbc435867
devmatheusfernandes <dev.matheusfernandes@gmail.com>
2026-04-06 01:32:46 -0300

    Fix

--- lib/firebase/firebase-admin.ts
+++ lib/firebase/firebase-admin.ts
@@ -1,9 +1,9 @@
 import * as admin from "firebase-admin";
 
-const projectId = process.env.FIREBASE_PROJECT_ID;
+const projectId = process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID;
 const clientEmail = process.env.FIREBASE_CLIENT_EMAIL;
 const privateKey = process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n").replace(/^"(.*)"$/, "$1");
-const storageBucket = process.env.FIREBASE_STORAGE_BUCKET;
+const storageBucket = process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET;
 
 if (!admin.apps.length) {
   if (!projectId || !clientEmail || !privateKey) {
